6769e7659cb77e50d24091bf3267e63d7e674c6f
Godfrey Best <godfrey.c.best@gmail.com>
2020-01-01 23:14:24 -0500

    update eslint and start createServer

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,9 @@
 import cookieParser from 'cookie-parser';
 import jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
+import { GraphQLServer } from 'graphql-yoga';
+import createServer from './createServer';
+import db from './db';
+
+config({ path: 'variables.env' });
+const server: GraphQLServer = createServer();
