47bafe01e889106c5be01519bd521fe6cedead0e
Godfrey Best <godfrey.c.best@gmail.com>
2020-01-05 09:30:04 -0500

    start on click for changing theme

--- frontend/components/Home.tsx
+++ frontend/components/Home.tsx
@@ -1,3 +1,10 @@
-const Home = () => <div>YERRRRR</div>;
+import Button from '@material-ui/core/Button';
+
+const Home = () => (
+    <div>
+        <Button color="primary">Primary</Button>
+        <Button color="secondary">Secondary</Button>
+    </div>
+);
 
 export default Home;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,17 +1,79 @@
-import * as injectTapEventPlugin from 'react-tap-event-plugin';
-// import injectTapEventPlugin from 'react-tap-event-plugin';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import { MuiThemeProvider, lightBaseTheme, darkBaseTheme } from 'material-ui/styles';
-import Home from '../components/Home';
+// import getMuiTheme from 'material-ui/styles/getMuiTheme';
+// import { MuiThemeProvider, lightBaseTheme, darkBaseTheme } from 'material-ui/styles';
+// import Home from '../components/Home';
 
-const lightMuiTheme = getMuiTheme(lightBaseTheme);
-const darkMuiTheme = getMuiTheme(darkBaseTheme);
-// injectTapEventPlugin();
+// const lightMuiTheme = getMuiTheme(lightBaseTheme);
+// const darkMuiTheme = getMuiTheme(darkBaseTheme);
 
-const HomePage = () => (
-    <MuiThemeProvider muiTheme={darkMuiTheme}>
-        <Home />
-    </MuiThemeProvider>
+// const HomePage = () => (
+//     // <MuiThemeProvider muiTheme={darkMuiTheme}>
+//     //     <Home />
+//     // </MuiThemeProvider>
+//     <MuiThemeProvider muiTheme={lightMuiTheme}>
+//         <Home />
+//     </MuiThemeProvider>
+// );
+
+// export default HomePage;
+
+import React, { useState } from 'react';
+import Typography from '@material-ui/core/Typography';
+import {
+    createStyles,
+    makeStyles,
+    ThemeProvider,
+    useTheme,
+    createMuiTheme,
+    Theme,
+} from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            padding: theme.spacing(3),
+            textAlign: 'center',
+            backgroundColor: theme.palette.background.default,
+            color: theme.palette.text.primary,
+        },
+    })
 );
 
-export default HomePage;
+const lightTheme = createMuiTheme({
+    palette: {
+        // This is the default, so only included for comparison.
+        type: 'light',
+    },
+});
+
+const darkTheme = createMuiTheme({
+    palette: {
+        // Switching the dark mode on is a single property value change.
+        type: 'dark',
+    },
+});
+
+function Demo() {
+    const defaultClasses = useStyles(darkTheme);
+    const theme = useTheme();
+    const [classes, setClasses] = useState({ darkMode: false });
+
+    return (
+        <div className={classes.root}>
+            <Typography>{`${theme.palette.type} theme`}</Typography>
+            <div>
+                <Button onClick={() => setClasses({ darkMode: classes })}>Toggle</Button>
+            </div>
+        </div>
+    );
+}
+
+export default function HomePage() {
+    return (
+        <div style={{ width: '100%' }}>
+            <ThemeProvider theme={darkTheme}>
+                <Demo />
+            </ThemeProvider>
+        </div>
+    );
+}
